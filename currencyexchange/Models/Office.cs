49740a032a225001b50f415ecb0bcb5a1f5b74e2
AlanMcKen <leo.pardus33@gmail.com>
2015-06-30 07:56:03 +0300

    Initial commit 2

---
+++ currencyexchange/Models/Office.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CurrencyExchange.Models
+{
+    public class Office
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int pcode { get; set; }
+        public int region {get; set;}
+        public string city { get; set; }
+        public string adress { get; set; }
+}
+    public class OfficeContext : DbContext
+    {
+        
+        //Контекст данных представляет собой класс, производный от класса DbContext. Контекст данных содержит одно или несколько свойств типа DbSet<T>, где T представляет тип объекта, хранящегося в базе данных.
+        public DbSet<Office> offices { get; set; } //свойство оffices
+    }
+
+}
